6cd15c07990f01ee8bb65eb2f61ca27cce60e1eb
pa-clysman <clysmanalves@plantaoativo.com>
2021-05-13 12:30:04 -0300

    Removing welcome in dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,7 +9,7 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+                    
                 </div>
             </div>
         </div>
@@ -18,12 +18,10 @@
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
 
     export default {
         components: {
             AppLayout,
-            Welcome,
         },
     }
 </script>
